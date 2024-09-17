51101166666b49b8041ed7470d2ad231b299de44
bek <shoyatbekov03032003@gmail.com>
2024-09-17 15:56:13 +0500

    file filters added

--- src/musics/musics.controller.ts
+++ src/musics/musics.controller.ts
@@ -1,5 +1,3 @@
-// src/music/music.controller.ts
-
 import { Controller, Get, Post, Body, Param, Delete, Render, UseInterceptors, UploadedFile, Res, HttpStatus, Query, ParseIntPipe } from '@nestjs/common';
 import { MusicsService } from './musics.service';
 import { FileInterceptor } from '@nestjs/platform-express';
@@ -7,20 +5,34 @@ import { Response } from 'express';
 import * as path from 'path';
 import { diskStorage } from 'multer';
 
+const AUDIO_MIME_TYPES = ["audio/aac", "audio/midi", "audio/ogg", "audio/wav"]
 @Controller('music')
 export class MusicsController {
   constructor(private readonly musicService: MusicsService) { }
 
   @Post()
   @UseInterceptors(FileInterceptor('file', {
+
+    fileFilter(req, file, cb) {
+      // Check if the file is an audio file and size is less than 5MB
+      if (AUDIO_MIME_TYPES.includes(file.mimetype) && file.size < 1024 * 1024 * 10) {
+        //correct format
+        return cb(null, true);
+      } else {
+        //wrong format
+        return cb(null, false);
+      }
+    },
     storage: diskStorage({
       destination: 'public',
+
       filename: (req, file, cb) => {
         // Generating a 32 random chars long string
         const randomName = Array(12).fill(null).map(() => (Math.round(Math.random() * 16)).toString(16)).join('')
         //Calling the callback passing the random name generated with the original extension name
         cb(null, `${randomName}${path.extname(file.originalname)}`)
-      }
+      },
+
     }),
   }))
   async create(@UploadedFile() file: Express.Multer.File, @Body('title') title: string, @Res() res: Response) {
