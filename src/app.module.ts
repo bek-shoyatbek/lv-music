6645c91651631c99a7ff95bdba40ff6130161586
bek <shoyatbekov03032003@gmail.com>
2024-09-17 15:18:17 +0500

    ConfigModule imported into app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MusicsModule } from './musics/musics.module';
 import { MulterModule } from '@nestjs/platform-express';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [MusicsModule,
     MulterModule.register({
       dest: './public',
-    })
+    }),
+    ConfigModule
   ],
   controllers: [AppController],
   providers: [AppService],
