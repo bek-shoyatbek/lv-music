053abe117d5bcb8a2852f49b416327d9c6609057
bek <shoyatbekov03032003@gmail.com>
2024-09-17 15:14:15 +0500

    PORT comes from .env

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const configService: ConfigService = app.get<ConfigService>(ConfigService);
+  const port = configService.get<number>("PORT");
 
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'src/views'));
   app.setViewEngine('ejs');
 
-  await app.listen(3000);
+  await app.listen(port);
 }
-bootstrap();
+bootstrap()
+  .then(res => {
+    console.log("Server is running...")
+  })
